131af2d20adbfeac449b5c57399db271fab44306
Julio Caro <jcarocota@gmail.com>
2025-04-01 20:43:35 -0700

    Update hello.py
    Se agregó un nuevo print

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-print("¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!!")
+print("¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!! perrito!!!! ")
+print("Ahora vengo de un hook")
